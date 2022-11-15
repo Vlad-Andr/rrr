f5d26a124954bb01cd0b3588e5d596b207b825f1
Vlad-Andr <V!@nDr%$1999>
2022-11-15 13:31:52 +0200

    prognoz

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world I wish you die later than 50000 after me!");
     }
 }
